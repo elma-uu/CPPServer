63e11a5999814ccbc9b92e8f48a93cd2ff0c86d9
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-02-23 20:17:47 +0900

    소수구하기 연습문제

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,34 +5,73 @@
 #include <mutex>
 #include <future>
 #include <windows.h>
-#include "CoreMacro.h"
 #include "ThreadManager.h"
 
+#include <vector>
+#include <thread>
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
+// 소수 구하기
+bool IsPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int CountPrime(int start, int end)
+{
+	int count = 0;
+
+	for (int number = start; number <= end; number++)
+	{
+		if (IsPrime(number))
+			count++;
+	}
+
+	return count;
+}
+
+// 1과 자기 자신으로만 나뉘면 소수라고 함
 
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	const int MAX_NUMBER = 100'0000;
+	// 1~MAX_NUMBER까지의 소수 개수
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	vector<thread> threads;
 
-	GThreadManager->Join();
+	// 1000 = 168
+	// 1'0000 = 1229
+	// 100'0000 = 79498
+
+	int coreCount = thread::hardware_concurrency();
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+
+	atomic<int> primeCount = 0;
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min((i + 1) * jobCount, MAX_NUMBER);
+
+		threads.push_back(thread([start, end, &primeCount]()
+			{
+				primeCount += CountPrime(start, end);
+			}));
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << primeCount << endl;
 }
