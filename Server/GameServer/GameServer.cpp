67bba46bd75811c8d540965fc02262502c3c39dc
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-02-23 20:37:50 +0900

    Referebce Counting 시작

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,71 +7,10 @@
 #include <windows.h>
 #include "ThreadManager.h"
 
-#include <vector>
-#include <thread>
 
-// 소수 구하기
-bool IsPrime(int number)
-{
-	if (number <= 1)
-		return false;
-	if (number == 2 || number == 3)
-		return true;
-
-	for (int i = 2; i < number; i++)
-	{
-		if ((number % i) == 0)
-			return false;
-	}
-
-	return true;
-}
-
-int CountPrime(int start, int end)
-{
-	int count = 0;
-
-	for (int number = start; number <= end; number++)
-	{
-		if (IsPrime(number))
-			count++;
-	}
-
-	return count;
-}
-
-// 1과 자기 자신으로만 나뉘면 소수라고 함
 
 int main()
 {
-	const int MAX_NUMBER = 100'0000;
-	// 1~MAX_NUMBER까지의 소수 개수
+	
 
-	vector<thread> threads;
-
-	// 1000 = 168
-	// 1'0000 = 1229
-	// 100'0000 = 79498
-
-	int coreCount = thread::hardware_concurrency();
-	int jobCount = (MAX_NUMBER / coreCount) + 1;
-
-
-	atomic<int> primeCount = 0;
-
-	for (int i = 0; i < coreCount; i++)
-	{
-		int start = (i * jobCount) + 1;
-		int end = min((i + 1) * jobCount, MAX_NUMBER);
-
-		threads.push_back(thread([start, end, &primeCount]()
-			{
-				primeCount += CountPrime(start, end);
-			}));
-	}
-
-	for (thread& t : threads)
-		t.join();
-
-	cout << primeCount << endl;
 }
