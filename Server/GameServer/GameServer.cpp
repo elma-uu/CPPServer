3d3c6efc23147b3818434655735a5bdc0161b220
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-02-26 04:32:48 +0900

    스마트 포인터 시작
    // 1) 이미 만들어진 클래스 대상으로 사용 불가
    // 2) 순환 (Cycle) 문제 (표준 SharedPtr도 생기는 문제)
    위와 같은 문제점있음. 고쳐야함

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,78 +9,14 @@
 
 #include "RefCounting.h"
 
-class Wraight : public RefCountable
-{
-public:
-	int _hp = 150;
-	int _posX = 0;
-	int _posY = 0;
-};
 
-using WraightRef = TSharedPtr<Wraight>;
-
-class Missile : public RefCountable
-{
-public:
-	void SetTarget(WraightRef target)
-	{
-		_target = target;
-		// 중간에 개입 가능
-		//target->AddRef();
-	}
-
-	bool Update()
-	{
-		if (_target == nullptr)
-			return true;
-
-		int posX = _target->_posX;
-		int posY = _target->_posY;
-
-		// TODO : 좇아간다
-
-		if(_target->_hp == 0)
-		{
-			//_target->ReleaseRef();
-			_target = nullptr;
-			return false;
-		}
-	}
-
-	Wraight* _target = nullptr;
-};
-
-
-using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	WraightRef wraight(new Wraight());
-	wraight->ReleaseRef();
-	MissileRef missile(new Missile());
-	missile->ReleaseRef();
+	// 1) 이미 만들어진 클래스 대상으로 사용 불가
+	// 2) 순환 (Cycle) 문제 (표준 SharedPtr도 생기는 문제)
 
-	missile->SetTarget(wraight);
 
-	// 레이스가 피격 당함
-	wraight->_hp = 0;
-	//delete wraight;
-	//wraight->ReleaseRef();
-	wraight = nullptr;
 
-	while (true)
-	{
-		if(missile)
-		{
-			if (missile->Update())
-			{
-				//missile->ReleaseRef();
-				missile = nullptr;
-			}
-		}
-	}
 
-	//missile->ReleaseRef();
-	missile = nullptr;
-	//delete missile;
 }
