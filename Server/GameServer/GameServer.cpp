fc9f01990776ce4c9a351b3f26ae161496e7a18b
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-02-27 19:56:48 +0900

    Cycle이 생기는 상황

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,6 +9,28 @@
 
 #include "RefCounting.h"
 
+using KnightRef = TSharedPtr<Knight>;
+
+class Knight : public RefCountable
+{
+public:
+	Knight()
+	{
+		cout << "Knight()" << endl;
+	}
+
+	~Knight()
+	{
+		cout << "~Knight()" << endl;
+	}
+
+	void SetTarget(KnightRef target)
+	{
+		_target = target;
+	}
+
+	KnightRef _target = nullptr;
+};
 
 
 int main()
@@ -16,7 +38,16 @@ int main()
 	// 1) 이미 만들어진 클래스 대상으로 사용 불가
 	// 2) 순환 (Cycle) 문제 (표준 SharedPtr도 생기는 문제)
 
+	KnightRef k1(new Knight());
+	k1->ReleaseRef();
+	KnightRef k2(new Knight());
+	k2->ReleaseRef();
 
+	k1->SetTarget(k2);
+	k2->SetTarget(k1);
+
+	k1 = nullptr;
+	k2 = nullptr;
 
 
 }
