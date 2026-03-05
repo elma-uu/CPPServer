f7bcc41089887b7d06e9761d1be496f97401ef29
elma_uu <ansvia@naver.com>
2026-03-05 12:51:44 +0900

    Allocator #1

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -23,10 +23,53 @@ public:
 		cout << "~Knight()" << endl;
 	}
 
+	static void* operator new(size_t size)
+	{
+		cout << "Knight new!" << size << endl;
+		void* ptr = ::malloc(size);
+		return ptr;
+	}
+
+	static void operator delete(void* ptr)
+	{
+		cout << "Knight delete!" << endl;
+		::free(ptr);
+	}
+
+	int32 _hp = 100;
+	int32 _mp = 10;
 };
 
+// new operator overloading (Global)
+void* operator new(size_t size)
+{
+	cout << "new!" << size << endl;
+	void* ptr = ::malloc(size);
+	return ptr;
+}
+
+void operator delete(void* ptr)
+{
+	cout << "delete!" << endl;
+	::free(ptr);
+}
+
+void* operator new[](size_t size)
+{
+	cout << "new[]!" << size << endl;
+	void* ptr = ::malloc(size);
+	return ptr;
+}
+
+void operator delete[](void* ptr)
+{
+	cout << "delete[]!" << endl;
+	::free(ptr);
+}
 
 int main()
 {
-	
+	Knight* knight = new Knight();
+
+	delete knight;
 }
